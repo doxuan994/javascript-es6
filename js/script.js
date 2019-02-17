0821afc388afd9467ea4b82945c976e57cc67f82
Xuan Do <doxuancntp@gmail.com>
2019-02-17 15:25:26 -0500

    Template Strings.

--- js/script.js
+++ js/script.js
@@ -58,24 +58,46 @@
 
 
 
+// window.onload = function() {
+//
+//     // var meats = ["ham", "salami", "bacon"];
+//     // console.log(...meats);
+//
+//     // var nums1 = [1,2,3];
+//     // var nums2 = [...nums1,4,5,6];
+//     //
+//     // console.log(nums2);
+//
+//
+//     var nums = [3, 5, 7];
+//
+//     function addNums(a, b, c) {
+//         console.log(a + b + c);
+//     }
+//
+//
+//     addNums(...nums);
+//
+// }
+
+
+
 window.onload = function() {
 
-    // var meats = ["ham", "salami", "bacon"];
-    // console.log(...meats);
-
-    // var nums1 = [1,2,3];
-    // var nums2 = [...nums1,4,5,6];
-    //
-    // console.log(nums2);
+    // var temp = `Hello, my name is Ryu`;
+    // console.log(temp);
 
 
-    var nums = [3, 5, 7];
+    function logNinja(name, age) {
+        // console.log("My name is" + name +
+        //             " and my age is " + age);
 
-    function addNums(a, b, c) {
-        console.log(a + b + c);
+
+        console.log(`My name is ${name} and my age is ${age + 10}`);
     }
 
 
-    addNums(...nums);
+
+    logNinja("Ryu", 24);
 
 }
