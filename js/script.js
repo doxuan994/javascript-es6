f50c1a2ead6c8acb69bb0362c3886ffef848e298
Xuan Do <doxuancntp@gmail.com>
2019-02-17 15:08:39 -0500

    The Let Keyword.

--- js/script.js
+++ js/script.js
@@ -1,13 +1,37 @@
-window.onload = function() {
-    const pi = 3.142;
+// window.onload = function() {
+//     const pi = 3.142;
+//
+//     function calcArea(r) {
+//         const pi = 10;
+//         console.log("The area is: " + pi * r * r);
+//     }
+//
+//     console.log(pi);
+//     calcArea(5)
+// }
 
-    function calcArea(r) {
-        const pi = 10;
-        console.log("The area is: " + pi * r * r);
+// window.onload = function() {
+//
+//     var x = 10;
+//
+//     if (x > 5) {
+//         let x = 5;
+//         console.log("inside: " + x);
+//     }
+//
+//     console.log("outside: " + x);
+//
+// }
+
+
+window.onload = function() {
+    var items = document.getElementsByTagName("li");
+
+    for (let i = 0; i < items.length; i++) {
+        items[i].onclick = function() {
+            console.log(i);
+        }
     }
 
-    console.log(pi);
-    calcArea(5)
-
-
+    // console.log(i);
 }
