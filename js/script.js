412aa052d84f4fbd8c129db11340b5c738b86a1d
Xuan Do <doxuancntp@gmail.com>
2019-02-17 14:59:24 -0500

    Constants.

---
+++ js/script.js
@@ -0,0 +1,13 @@
+window.onload = function() {
+    const pi = 3.142;
+
+    function calcArea(r) {
+        const pi = 10;
+        console.log("The area is: " + pi * r * r);
+    }
+
+    console.log(pi);
+    calcArea(5)
+
+
+}
